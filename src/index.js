593f0418cea5f936466c0f4740ec550fe234bb83
DevTabibur <tobiburrohman2@gmail.com>
2022-03-19 08:57:19 +0600

    initial step done

--- src/index.js
+++ src/index.js
@@ -4,11 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// component
+function Card(props){
+  console.log(props)
+  return(
+    <>
+    <div className='cards'>
+      <div className='card'>
+        <img src={props.imgSrc} alt="myPic" className='card__img' />
+        <div className='card__info'>
+          <span className='card__category'>{props.category}</span>
+          <h3 className='card__title'>{props.title}</h3>
+          <a href="" target="_blank">
+            <button> Watch Now </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+ReactDOM.render( <>
+  <Card imgSrc="https://i.pinimg.com/564x/bd/9b/e9/bd9be9cfc97ba73c66f51e927f2bfcc5.jpg"
+    title="DARK"
+    category="A Netflix Original Series"
+    href=""
+  />
+  <Card imgSrc=""/>
+  <Card imgSrc=""/>
+</> ,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
